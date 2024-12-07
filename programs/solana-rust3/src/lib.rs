c4db8a05b9407d381fa7317e0d97a81f34a4af17
hoangtv2101 <hoangtv2101@gmail.com>
2024-12-08 00:21:12 +0700

    dummy

--- programs/solana-rust3/src/lib.rs
+++ programs/solana-rust3/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("AgLd6tvSA9J3PH91KikYxNMRYqLQMi5aSKyofGECuuSP");
+declare_id!("3YZmiD8PjFPG7vTva9MtPF2FBw4aRUmho5kXKTYj9cFM");
 
 #[program]
 pub mod solana_rust3 {
